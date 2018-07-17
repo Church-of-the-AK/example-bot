9c433e4b1c61b5a701d46f70dca5a87d64dcc182
jasonhaxstuff <shadowmanguyyt@gmail.com>
2018-07-17 12:13:36 -0400

    use RichEmbed

--- commands/member/suggest.ts
+++ commands/member/suggest.ts
@@ -1,7 +1,6 @@
 import * as commando from 'discord.js-commando'
 import { oneLine } from 'common-tags'
-import ec = require('embed-creator')
-import { Message, TextChannel } from 'discord.js';
+import { Message, TextChannel, RichEmbed } from 'discord.js';
 
 module.exports = class SuggestCommand extends commando.Command {
   constructor(client) {
@@ -22,60 +21,40 @@ module.exports = class SuggestCommand extends commando.Command {
         key: 'suggestion',
         label: 'suggestion',
         prompt: 'What would you like to suggest?`',
-        type: 'string',
-        infinite: false
+        type: 'string'
       }]
     })
   }
 
-  async run(msg: commando.CommandMessage, { suggestion }): Promise<Message> {
-    if (msg.channel.type == "text") {
-      try {
-        let channel = msg.guild.channels.find('name', 'suggestions') as TextChannel
-        if (channel) {
-          const suggestionMsg = await channel.send(ec(
-            "#FEAFEA", {
-              "name": msg.author.username,
-              "icon_url": msg.author.displayAvatarURL,
-              "url": null
-            }, "New Suggestion", suggestion, [], {
-              "text": "MachoBot",
-              "icon_url": null
-            }, {
-              "thumbnail": null,
-              "image": null
-            }, false
-          )) as Message
+  async run(msg: commando.CommandMessage, { suggestion }): Promise<Message | Message[]> {
+    try {
+      const channel = msg.guild.channels.find('name', 'suggestions') as TextChannel
 
-          await suggestionMsg.react('👍')
-          suggestionMsg.react('👎')
+      if (!channel) {
+        await msg.reply("This server has no #suggestions channel.")
+        return msg.delete()
+      }
 
-          const reply = await msg.reply("Suggestion added!")
-          if (reply instanceof Message) {
-            reply.delete(3000)
-            msg.delete()
-          }
-        } else {
-          const reply = await msg.reply("This server has no #suggestions channel.")
-          if (reply instanceof Message) {
-            reply.delete(3000)
-            msg.delete()
-          }
-        }
-      } catch (err) {
-        const reply = await msg.reply("Oops can't do that srry bye now")
-        if (reply instanceof Message) {
-          reply.delete(3000)
-          msg.delete()
-        }
-      }
-    } else {
-      const reply = await msg.reply("This command can only be used in a server.")
-      if (reply instanceof Message) {
-        reply.delete(3000)
-        msg.delete()
-      }
+      const embed = new RichEmbed()
+        .setAuthor(msg.author.username)
+        .setThumbnail(msg.author.displayAvatarURL)
+        .setTimestamp(new Date())
+        .setColor('BLUE')
+        .setTitle('New Suggestion')
+        .setFooter('Macho')
+        .setDescription(suggestion)
+
+      const suggestionMsg = await channel.send(embed) as Message
+
+      await suggestionMsg.react('👍')
+      await suggestionMsg.react('👎')
+
+      const reply = await msg.reply("Suggestion added!")
+
+      return msg.delete()
+    } catch (err) {
+      await msg.reply("Oops can't do that srry bye now")
+      return msg.delete()
     }
-    return undefined
   }
 }
