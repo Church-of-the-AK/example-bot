3f49a9ff228650494b2f9bbaf3518f07a53edd0c
Jason Bothel <shadowmanguyyt@gmail.com>
2018-09-02 12:09:43 -0400

    log

--- src/handlers/VoiceStateUpdateHandler.ts
+++ src/handlers/VoiceStateUpdateHandler.ts
@@ -22,6 +22,7 @@ export async function handleVoiceStateUpdate (oldMember: GuildMember, newMember:
   if (oldVoice && !newVoice && oldVoice.id === serverQueue.voiceChannel.id) {
     if (serverQueue.voiceChannel.members.size === 0) {
       if (serverQueue.playing) {
+        console.log('Pausing')
         serverQueue.playing = false
         serverQueue.connection.dispatcher.pause()
         serverQueue.textChannel.send('All users have left the voice channel. The music has been paused.')
@@ -29,6 +30,7 @@ export async function handleVoiceStateUpdate (oldMember: GuildMember, newMember:
     }
   } else if (!oldVoice && newVoice && newVoice.id === serverQueue.voiceChannel.id) {
     if (!serverQueue.playing) {
+      console.log('Playing')
       serverQueue.playing = true
       serverQueue.connection.dispatcher.resume()
     }
