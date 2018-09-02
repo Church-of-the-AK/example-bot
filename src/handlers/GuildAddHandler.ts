021cb4d0c20efdf93dc09ef308dc830af036d6e4
Jason Bothel <shadowmanguyyt@gmail.com>
2018-09-02 12:59:48 -0400

    create guilds

--- src/handlers/GuildAddHandler.ts
+++ src/handlers/GuildAddHandler.ts
@@ -1,6 +1,8 @@
 import { CommandoGuild } from 'discord.js-commando'
 import { createUser, getUser } from '../util'
 import { Guild, GuildSettings } from 'machobot-database'
+import { api } from '../config'
+import axios from 'axios'
 
 export async function handleGuildAdd (guild: CommandoGuild) {
   console.log(`Joined guild ${guild.name} (${guild.id})`)
@@ -20,8 +22,11 @@ export async function handleGuildAdd (guild: CommandoGuild) {
   })
 
   const apiGuild = new Guild()
+
   apiGuild.id = guild.id
   apiGuild.name = guild.name
   apiGuild.banned = false
   apiGuild.settings = new GuildSettings()
+
+  await axios.post(`${api.url}/guilds`, apiGuild)
 }
