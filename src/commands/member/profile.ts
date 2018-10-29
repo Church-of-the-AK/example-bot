06b7ba3aa684a5820175368c8516b915d56bbb6a
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-28 22:44:28 -0400

    url

--- src/commands/member/profile.ts
+++ src/commands/member/profile.ts
@@ -3,7 +3,6 @@ import * as Canvas from 'canvas'
 import { Message, MessageAttachment } from 'discord.js'
 import { MachoCommand } from '../../types'
 import { getUser } from '../../util'
-import axios from 'axios'
 
 export default class ProfileCommand extends MachoCommand {
   constructor (client) {
@@ -35,9 +34,7 @@ export default class ProfileCommand extends MachoCommand {
     const canvas = Canvas.createCanvas(700, 250)
     const ctx = canvas.getContext('2d')
     const background = await Canvas.loadImage('images/background.jpeg')
-    const { data: arrayBuffer }: { data: ArrayBuffer } = await axios.get(user.avatarUrl, { responseType: 'arraybuffer' })
-    const buffer = Buffer.from(arrayBuffer)
-    const avatar = await Canvas.loadImage(buffer)
+    const avatar = await Canvas.loadImage(user.avatarUrl)
 
     ctx.drawImage(avatar, 25, 25, 200, 200)
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
